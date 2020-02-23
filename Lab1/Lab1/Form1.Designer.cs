ab1cb769645f25f0245b96d83505b644a1244b4d
Kirill Emets <kirill.yemets@gmail.com>
2020-02-23 20:45:37 +0200

    First commit

---
+++ Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMatrix = new System.Windows.Forms.TextBox();
+            this.cbArrows = new System.Windows.Forms.CheckBox();
+            this.tbMatrixUnDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackScale = new System.Windows.Forms.TrackBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackScale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbMatrix
+            // 
+            this.tbMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMatrix.Location = new System.Drawing.Point(12, 12);
+            this.tbMatrix.Multiline = true;
+            this.tbMatrix.Name = "tbMatrix";
+            this.tbMatrix.ReadOnly = true;
+            this.tbMatrix.Size = new System.Drawing.Size(170, 213);
+            this.tbMatrix.TabIndex = 0;
+            // 
+            // cbArrows
+            // 
+            this.cbArrows.AutoSize = true;
+            this.cbArrows.Location = new System.Drawing.Point(188, 12);
+            this.cbArrows.Name = "cbArrows";
+            this.cbArrows.Size = new System.Drawing.Size(138, 17);
+            this.cbArrows.TabIndex = 1;
+            this.cbArrows.Text = "Малюнок зі стрілками";
+            this.cbArrows.UseVisualStyleBackColor = true;
+            this.cbArrows.CheckedChanged += new System.EventHandler(this.cbArrows_CheckedChanged);
+            // 
+            // tbMatrixUnDir
+            // 
+            this.tbMatrixUnDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMatrixUnDir.Location = new System.Drawing.Point(12, 288);
+            this.tbMatrixUnDir.Multiline = true;
+            this.tbMatrixUnDir.Name = "tbMatrixUnDir";
+            this.tbMatrixUnDir.ReadOnly = true;
+            this.tbMatrixUnDir.Size = new System.Drawing.Size(170, 213);
+            this.tbMatrixUnDir.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Матриця напрямленого графа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Матриця ненапрямленого графа";
+            // 
+            // trackScale
+            // 
+            this.trackScale.LargeChange = 1;
+            this.trackScale.Location = new System.Drawing.Point(188, 51);
+            this.trackScale.Minimum = 1;
+            this.trackScale.Name = "trackScale";
+            this.trackScale.Size = new System.Drawing.Size(138, 45);
+            this.trackScale.TabIndex = 5;
+            this.trackScale.Value = 1;
+            this.trackScale.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.vScrollBar1.Location = new System.Drawing.Point(775, 9);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(21, 475);
+            this.vScrollBar1.TabIndex = 6;
+            this.vScrollBar1.Value = 50;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(188, 484);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(608, 17);
+            this.hScrollBar1.TabIndex = 7;
+            this.hScrollBar1.Value = 50;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 513);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.trackScale);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMatrixUnDir);
+            this.Controls.Add(this.cbArrows);
+            this.Controls.Add(this.tbMatrix);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackScale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMatrix;
+        private System.Windows.Forms.CheckBox cbArrows;
+        private System.Windows.Forms.TextBox tbMatrixUnDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackScale;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        const int n1 = 9;
+        const int n2 = 5;
+        const int n3 = 0;
+        const int n4 = 7;
+        public Node centerNode;
+
+        List<Node> nodes;
+        int[,] matrix;
+
+        bool withArrows = false;
+        float drawScale = 1;
+        Vector2 transformOffset = new Vector2(0, 0);
+        Vector2 scaleOffset = new Vector2(0, 0);
+
+        int nodeRadius = 30;
+        int bigRadius = 150;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // початок програми
+        {
+            nodes = CreateNodes(10, new Vector2(500, 200), bigRadius); // створення вершин
+            centerNode = nodes[0];
+            matrix = GenerateMatrix(10); // створення матриці
+
+            WriteMatrix(matrix, tbMatrix); // вивід матриць
+            WriteMatrix(undirectedMatrix(matrix), tbMatrixUnDir);
+
+            Invalidate(); // відрисовка графу
+        }
+
+        protected override void OnPaint(PaintEventArgs e) // відрисовка графу
+        {
+            base.OnPaint(e);
+            //Вариант 9507
+            Graphics g = e.Graphics;
+
+            g.TranslateTransform(transformOffset.x + scaleOffset.x, transformOffset.y + scaleOffset.y);
+            g.ScaleTransform(drawScale, drawScale);
+            Pen penNode = new Pen(Brushes.Black, 2);
+            Pen penLines = new Pen(Brushes.Black, 1);
+
+            foreach (var n in nodes)
+            {
+                n.Draw(penNode, g); // відрисовка вершин
+            }
+
+            DrawLinesFromMatrix(matrix, nodes, penLines, g, withArrows); // відрисовка ліній і стрілок
+        }
+
+        List<Node> CreateNodes(int count, Vector2 circleCenter, int circleRadius)
+        {
+            // коло з вершиною в центрі
+
+            List<Node> nodes = new List<Node>();
+            nodes.Add(new Node(circleCenter.x, circleCenter.y, nodeRadius, 0));
+
+            for (int i = 1; i < count; i++)
+            {
+                float angle = (float)(2 * Math.PI * i / (count - 1));
+                Vector2 pos = circleCenter + circleRadius * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                nodes.Add(new Node(pos.x, pos.y, nodeRadius, i));
+            }
+
+            return nodes;
+        }
+
+        int[,] GenerateMatrix(int n)
+        {
+            Random rand = new Random(n1 * 1000 + n2 * 100 + n3 * 10 + n4);
+
+            int[,] m1 = randomMatrix(rand, n);
+            int[,] m2 = randomMatrix(rand, n);
+
+            int[,] T = new int[n, n]; 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    T[i, j] = m1[i, j] + m2[i, j];
+                    T[i, j] = (int)Math.Floor(T[i,j]*(1.0 - n3*0.02 - n4*0.005 - 0.25));
+                }
+            }
+            return T;
+        }
+
+        int[,] randomMatrix(Random rand, int n)
+        {
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rand.Next(2);
+                }
+            }
+            return matrix;
+        }
+
+        int[,] undirectedMatrix(int[,] m)
+        {
+            int[,] res = m.Clone() as int[,];
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    if (res[i, j] == 1)
+                        res[j, i] = 1;
+                }
+            }
+
+            return res;
+        }
+
+        void WriteMatrix(int[,] m, TextBox tb)
+        {
+            tb.Text = "";
+            string s = "";
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                s = "";
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    s += m[i, j] + " ";
+                }
+                tb.AppendText(s + "\r\n");
+            }
+            tb.Text = tb.Text.Remove(tb.Text.LastIndexOf("\r\n"));
+        }
+
+        void DrawLinesFromMatrix(int[,] matrix, List<Node> nodes, Pen pen, Graphics g, bool withArrows)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j] == 1)
+                    {
+                        if (i == j)
+                        {
+                            nodes[i].DrawCircle(centerNode.center, pen, g, withArrows);
+                        }
+
+                        else
+                        {
+                            bool isSecond = matrix[j, i] == 1 && i > j && withArrows;
+                            Vector2 cp = null;
+                            Node obstacle = null;
+                            foreach (var n in nodes)
+                            {
+                                cp = n.isBetween(nodes[i], nodes[j]);
+                                if (cp != null)
+                                {
+                                    obstacle = n;
+                                    break;
+                                }
+                            }
+
+                            if (cp != null)
+                            {
+                                float scale = isSecond ? 1.7f : 1.3f;
+                                Vector2 op = obstacle.GetOffsetPointOnCircle(cp, scale);
+                                nodes[i].DrawArcTo(nodes[j], op, pen, g, withArrows);
+                            }
+
+                            else
+                            {
+                                if (isSecond)
+                                {
+                                    Vector2 op = findFreePointBetween(nodes[i].center, nodes[j].center);
+                                    nodes[i].DrawArcTo(nodes[j], op, pen, g, withArrows);
+                                }
+                                else
+                                    nodes[i].DrawLineTo(nodes[j], pen, g, withArrows);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Vector2 findFreePointBetween(Vector2 p1, Vector2 p2)
+        {
+            Vector2 dir = (p2 - p1).normalized;
+            Vector2 cp = (p1 + p2) / 2;
+            float h = 15;
+
+            Vector2 t1 = (cp + dir.turnOn((float)(1.57f)) * h);
+            bool isOk = true;
+
+            foreach (var node in nodes)
+            {
+                if (node.ContainsPoint(t1))
+                    isOk = false;
+            }    
+            
+            if(!isOk)
+                t1 = (cp - dir.turnOn((float)(1.57f)) * h);
+            return t1;
+        }
+
+        private void cbArrows_CheckedChanged(object sender, EventArgs e)
+        {
+            withArrows = cbArrows.Checked;
+            
+            Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            drawScale = 1 + (sender as TrackBar).Value / 10f;
+            transformOffset = new Vector2(2, 1) * (drawScale - 1) * -300;
+
+            Invalidate();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            scaleOffset.x = -(hScrollBar1.Value - 50) * 10;
+            Invalidate();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            scaleOffset.y = -(vScrollBar1.Value - 50) * 10;
+            Invalidate();
+        }
+    }
+
+    public class Node
+    {
+        Vector2 position;
+        float radius;
+        public int index;
+        public Rectangle rect;
+        public Vector2 center
+        {
+            
+            get { return position + new Vector2(rect.Width, rect.Height) / 2; }
+        }
+        
+
+        public Node(float x, float y, int radius, int n)
+        {        
+            index = n;
+            rect = new Rectangle((int)x, (int)y, 2 * radius, 2 * radius);
+            this.radius = radius;
+            position = Vector2.FromPoint(rect.Location);
+        }
+
+        public void Draw(Pen pen, Graphics g)
+        {
+            
+            g.FillEllipse(Brushes.Pink, rect);
+            g.DrawEllipse(pen, rect);
+            g.DrawString((index+1) + "", new Font("Arial", 12, FontStyle.Bold), Brushes.Black, (center - new Vector2(7, 7)).point);
+        }
+
+        public void DrawCircle(Vector2 centerPoint, Pen pen, Graphics g, bool withArrow)
+        {
+            Vector2 dir = (center - centerPoint).normalized;
+            Vector2 p = center + dir * 2 * radius;
+
+            Vector2 p2 = p + dir.turnOn((float)(1.57f)) * 30;
+            Vector2 p3 = p - dir.turnOn((float)(1.57f)) * 30;
+
+            Vector2 p1 = Vector2.FromPoint(GetEdgePoint(p2));
+            Vector2 p4 = Vector2.FromPoint(GetEdgePoint(p3));
+
+            g.DrawBezier(pen, p1.point, p2.point, p3.point, p4.point);
+
+            if (withArrow)
+            {
+                dir = (p3 - p4).normalized;
+                const int h = 10;
+                Point t1 = p4.point;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((float)(1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((float)(1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] { t1, t2, t3 });
+            }
+        }
+
+        public void DrawLineTo(Node node, Pen pen, Graphics g, bool withArrow)
+        {
+            Point p1 = GetEdgePoint(node.center);
+            Point p2 = node.GetEdgePoint(this.center);
+            g.DrawLine(pen, p1, p2);
+
+            if (withArrow)
+            {
+                Vector2 dir = (center - node.center).normalized;
+                const int h = 10;
+                Point t1 = p2;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((float)(1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((float)(1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] {t1, t2, t3});
+            }
+        }
+
+        public void DrawArcTo(Node node, Vector2 op, Pen pen, Graphics g, bool withArrow)
+        {
+            Point p1 = GetEdgePoint(op);
+            Point p2 = node.GetEdgePoint(op);
+
+            g.DrawBezier(pen, p1, op.point, op.point, p2);
+
+            if (withArrow)
+            {
+                Vector2 dir = (op - node.center).normalized;
+                const int h = 10;
+                Point t1 = p2;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((float)(1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((float)(1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] { t1, t2, t3 });
+            }
+        }
+
+        public Vector2 isBetween(Node n1, Node n2)
+        {
+            Vector2 cp = (n2.center + n1.center) / 2;
+            if((cp - center).length < radius)
+                return cp;
+
+            return null;
+        }
+
+        public Vector2 GetOffsetPointOnCircle(Vector2 point, float scale)
+        {
+            if (ContainsPoint(point))
+            {
+                Vector2 dir = (point - center).normalized;
+                Vector2 op = center + dir * (radius * scale);
+                return op;
+            }
+            return null;
+        }
+
+        public bool ContainsPoint(Vector2 p)
+        {
+            if ((center - p).length <= radius)
+                return true;
+
+            return false;
+        }
+
+        public Point GetEdgePoint(Vector2 point)
+        {
+            return (center + (point - center).normalized * radius).point;
+        }
+    }
+}
