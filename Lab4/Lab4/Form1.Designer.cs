c300e3d69341eb7fa7ec02239789d89fd03df74c
Kirill Emets <kirill.yemets@gmail.com>
2020-04-23 22:18:24 +0300

    Lab 4 added

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbMatrix = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nodeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btn_bfsNext = new System.Windows.Forms.Button();
+            this.btn_Again = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_byPass = new System.Windows.Forms.TextBox();
+            this.tb_ratio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbMatrix
+            // 
+            this.tbMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMatrix.Location = new System.Drawing.Point(12, 12);
+            this.tbMatrix.Multiline = true;
+            this.tbMatrix.Name = "tbMatrix";
+            this.tbMatrix.ReadOnly = true;
+            this.tbMatrix.Size = new System.Drawing.Size(170, 213);
+            this.tbMatrix.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Матриця напрямленого графа";
+            // 
+            // btn_bfsNext
+            // 
+            this.btn_bfsNext.Location = new System.Drawing.Point(188, 12);
+            this.btn_bfsNext.Name = "btn_bfsNext";
+            this.btn_bfsNext.Size = new System.Drawing.Size(75, 23);
+            this.btn_bfsNext.TabIndex = 4;
+            this.btn_bfsNext.Text = "Next";
+            this.btn_bfsNext.UseVisualStyleBackColor = true;
+            this.btn_bfsNext.Click += new System.EventHandler(this.btn_bfsNext_Click);
+            // 
+            // btn_Again
+            // 
+            this.btn_Again.Location = new System.Drawing.Point(269, 12);
+            this.btn_Again.Name = "btn_Again";
+            this.btn_Again.Size = new System.Drawing.Size(75, 23);
+            this.btn_Again.TabIndex = 5;
+            this.btn_Again.Text = "Again";
+            this.btn_Again.UseVisualStyleBackColor = true;
+            this.btn_Again.Click += new System.EventHandler(this.btn_Again_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Матриця дерева обходу";
+            // 
+            // tb_byPass
+            // 
+            this.tb_byPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_byPass.Location = new System.Drawing.Point(15, 290);
+            this.tb_byPass.Multiline = true;
+            this.tb_byPass.Name = "tb_byPass";
+            this.tb_byPass.ReadOnly = true;
+            this.tb_byPass.Size = new System.Drawing.Size(170, 213);
+            this.tb_byPass.TabIndex = 8;
+            // 
+            // tb_ratio
+            // 
+            this.tb_ratio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_ratio.Location = new System.Drawing.Point(680, 9);
+            this.tb_ratio.Multiline = true;
+            this.tb_ratio.Name = "tb_ratio";
+            this.tb_ratio.ReadOnly = true;
+            this.tb_ratio.Size = new System.Drawing.Size(170, 213);
+            this.tb_ratio.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(677, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Матриця відповідності";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(677, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(номер в дереві - номер в графі)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 513);
+            this.Controls.Add(this.tb_ratio);
+            this.Controls.Add(this.tb_byPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Again);
+            this.Controls.Add(this.btn_bfsNext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMatrix);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nodeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMatrix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource nodeBindingSource;
+        private System.Windows.Forms.Button btn_bfsNext;
+        private System.Windows.Forms.Button btn_Again;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_byPass;
+        private System.Windows.Forms.TextBox tb_ratio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,540 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        const int n1 = 9;
+        const int n2 = 5;
+        const int n3 = 0;
+        const int n4 = 7;
+        public Node centerNode;
+
+        Graph mainGraph;
+        Graph treeGraph;
+
+        Matrix ratioMatrix;
+
+        bool withArrows = true;
+
+        float drawScale = 1;
+        Vector2 transformOffset = new Vector2(0, 0);
+        Vector2 scaleOffset = new Vector2(0, 0);
+
+        int nodeRadius = 30;
+        int bigRadius = 150;
+
+        bool bfsFinished = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // початок програми
+        {
+            List<Node> nodes = createNodes(10 + n3, new Vector2(400, 200), bigRadius); // створення вершин
+            Matrix matrix = generateMatrix(10 + n3); // створення матриці
+            mainGraph = new Graph(nodes, matrix);
+            treeGraph = new Graph(nodes, new Matrix(matrix.n));
+
+            centerNode = nodes[0];
+
+            writeMatrix(matrix, tbMatrix); // вивід матриць
+
+            bfsStart();
+
+            Invalidate(); // відрисовка графу
+        }
+
+        protected override void OnPaint(PaintEventArgs e) // відрисовка графу
+        {
+            base.OnPaint(e);
+            //Вариант 9507
+            Graphics g = e.Graphics;
+
+            g.TranslateTransform(transformOffset.x + scaleOffset.x, transformOffset.y + scaleOffset.y);
+            g.ScaleTransform(drawScale, drawScale);
+            Pen penNode = new Pen(Brushes.Black, 2);
+            Pen penLines = new Pen(Brushes.Black, 1);
+
+            Graph graphToDraw = bfsFinished ? treeGraph : mainGraph;
+            drawGraph(graphToDraw, penLines, penNode, g, withArrows); // відрисовка ліній і стрілок
+        }
+
+        List<Node> createNodes(int count, Vector2 circleCenter, int circleRadius)
+        {
+            // коло з вершиною в центрі
+
+            List<Node> nodes = new List<Node>();
+            nodes.Add(new Node(circleCenter.x, circleCenter.y, nodeRadius, 0, "1"));
+
+            for (int i = 1; i < count; i++)
+            {
+                float angle = (float)(2 * Math.PI * i / (count - 1));
+                Vector2 pos = circleCenter + circleRadius * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                nodes.Add(new Node(pos.x, pos.y, nodeRadius, i, (i + 1) + ""));
+            }
+
+            return nodes;
+        }
+
+        Matrix generateMatrix(int n)
+        {
+            Random rand = new Random(n1 * 1000 + n2 * 100 + n3 * 10 + n4);
+            int[,] m1 = randomMatrix(rand, n);
+            int[,] m2 = randomMatrix(rand, n);
+
+            int[,] T = new int[n, n];
+            float k = 1.5f;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    T[i, j] = m1[i, j] + m2[i, j];
+                    //T[i, j] = 1;    
+                    T[i, j] = (int)Math.Floor(T[i,j]*(1.0 - n3*0.001 - n4*0.005 - 0.15));             
+                    //T[i, j] = (int)Math.Floor(T[i, j] * (1.0 - n3 * 0.001 - n4 * 0.005 - 0.15) * rand.NextDouble() * k);
+
+                    if (T[i, j] > 1) T[i, j] = 1;
+                }
+            }
+            return new Matrix(T);
+        }
+
+        int[,] randomMatrix(Random rand, int n)
+        {
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rand.Next(2);
+                }
+            }
+            return matrix;
+        }
+
+        Matrix undirectedMatrix(Matrix mx)
+        {
+            int[,] res = mx.values.Clone() as int[,];
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    if (res[i, j] == 1)
+                        res[j, i] = 1;
+                }
+            }
+
+            return new Matrix(res);
+        }
+
+        void writeMatrix(Matrix mx, TextBox tb)
+        {
+            int[,] m = mx.values;
+            tb.Text = "";
+            string s = "";
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                s = "";
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    s += m[i, j] + " ";
+                }
+                tb.AppendText(s + "\r\n");
+            }
+            tb.Text = tb.Text.Remove(tb.Text.LastIndexOf("\r\n"));
+        }
+
+        void drawGraph(Graph graph, Pen penLines, Pen penNode, Graphics g, bool withArrows)
+        {
+            Matrix mx = graph.matrix;
+            List<Node> nodes = graph.nodes;
+
+            foreach (var n in nodes)
+            {
+                n.Draw(penNode, g); // відрисовка вершин
+            }
+
+            int[,] matrix = mx.values;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j] == 1)
+                    {
+                     
+                        if (i == j)
+                        {
+                            nodes[i].DrawCircle(centerNode.center, penLines, g, withArrows);
+                        }
+                        
+                        else
+                        {
+                            bool isSecond = matrix[j, i] == 1 && i > j && withArrows;
+                            Vector2 cp = null;
+                            Node obstacle = null;
+
+
+                            foreach (var n in nodes)
+                            {
+                                cp = n.isBetween(nodes[i], nodes[j]);
+                                if (cp != null)
+                                {
+                                    obstacle = n;
+                                    break;
+                                }
+                            }
+
+                            
+
+                            if (cp != null)
+                            {
+                                float scale = isSecond ? 1.7f : 1.4f;
+                                Vector2 op = obstacle.GetOffsetPointOnCircle(cp, scale);
+                                nodes[i].DrawArcTo(nodes[j], op, penLines, g, withArrows);
+                            }
+
+                            else
+                            {
+                                if (isSecond)
+                                {
+                                    Vector2 op = findFreePointBetween(nodes[i].center, nodes[j].center);
+                                    nodes[i].DrawArcTo(nodes[j], op, penLines, g, withArrows);
+                                }
+                                else
+                                    nodes[i].DrawLineTo(nodes[j], penLines, g, withArrows);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void calculatePowerOfMatrix(Matrix mx, List<Node> nodes)
+        {
+            int[,] matrix = mx.values;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        nodes[i].hpowOut += 1;
+                        nodes[j].hpowIn += 1;
+
+                        nodes[i].power += 1;
+                        nodes[j].power += 1;
+                        if (matrix[j, i] == 1 && i > j) nodes[i].power -= 1;
+                        if (matrix[j, i] == 1 && i > j) nodes[j].power -= 1;
+                    }
+                }
+            }
+        } 
+
+        Vector2 findFreePointBetween(Vector2 p1, Vector2 p2)
+        {
+            Vector2 dir = (p2 - p1).normalized;
+            Vector2 cp = (p1 + p2) / 2;
+            float h = 15;
+
+            Vector2 t1 = (cp + dir.turnOn((float)(1.57f)) * h);
+            bool isOk = true;
+
+            foreach (var node in mainGraph.nodes)
+            {
+                if (node.ContainsPoint(t1))
+                    isOk = false;
+            }    
+            
+            if(!isOk)
+                t1 = (cp - dir.turnOn((float)(1.57f)) * h);
+            return t1;
+        }
+
+        List<int> opens;
+        List<int> closed;
+
+        int current = 0;
+        int treeIndex = 0;
+
+        void bfsStart()
+        {
+            foreach(var n in mainGraph.nodes)
+            {
+                n.current = false;
+                n.visited = false;
+                n.opened = false;
+                n.SetTreeIndex(-1);
+            }
+
+            closed = new List<int>();
+            opens = new List<int>();
+            ratioMatrix = new Matrix(mainGraph.matrix.n);
+
+            bfsFinished = false;
+            opens.Add(0);
+
+            current = 0;
+            makeNodeCurrent(0);
+            treeIndex = 0;
+            mainGraph.nodes[current].SetTreeIndex(0);
+            treeIndex++;
+
+            Invalidate();
+        }
+
+        public void bfsNext()
+        {
+            int[,] matrix = mainGraph.matrix.values;
+            for (int i = 0; i < mainGraph.matrix.n; i++)
+            {
+                if (matrix[current, i] == 1 && !closed.Contains(i) && !opens.Contains(i))
+                {
+                    openNode(i);
+                    treeGraph.matrix.values[current, i] = 1;
+                    mainGraph.nodes[i].SetTreeIndex(treeIndex);
+                    treeIndex++;
+                }
+            }
+            closeNode(current);
+            if (opens.Count > 0)
+            {
+                current = opens[0];
+                makeNodeCurrent(current);
+            }
+            else
+            {
+                bfsFinished = true;
+                mainGraph.nodes[current].current = false;
+                writeMatrix(treeGraph.matrix, tb_byPass);
+                writeMatrix(getRatioMatrix(), tb_ratio);
+            }
+
+            Invalidate();
+        }
+
+        void openNode(int index)
+        {
+            opens.Add(index);
+            mainGraph.nodes[index].opened = true;
+        }
+
+        void closeNode(int index)
+        {
+            closed.Add(index);
+            opens.Remove(index);
+        }
+
+        void makeNodeCurrent(int index)
+        {
+            foreach (var n in mainGraph.nodes)
+                n.current = false;
+
+            mainGraph.nodes[index].current = true;
+            mainGraph.nodes[index].visited = true;
+        }
+
+        Matrix getRatioMatrix()
+        {
+            Matrix res = new Matrix(mainGraph.matrix.n);
+            foreach (var node in mainGraph.nodes)
+            {
+                res.values[node.treeIndex, node.index] = 1;
+            }
+            return res;
+        }
+
+        private void btn_bfsNext_Click(object sender, EventArgs e)
+        {
+            bfsNext();
+        }
+
+        private void btn_Again_Click(object sender, EventArgs e)
+        {
+            bfsStart();
+        }
+    }
+
+    public class Node
+    {
+        float radius;
+
+        public Vector2 position;
+        public int index;
+        public string text;
+        public Rectangle rect;
+        public Vector2 center
+        {            
+            get { return position + new Vector2(rect.Width, rect.Height) / 2; }
+        }
+
+        public int hpowIn = 0;
+        public int hpowOut = 0;
+        public int power = 0;
+
+        public bool visited = false;
+        public bool opened = false;
+        public bool current = false;
+        public int treeIndex = -1;
+
+        public Node(float x, float y, int radius, int n, string text)
+        {        
+            index = n;
+            this.text = text;
+            rect = new Rectangle((int)x, (int)y, 2 * radius, 2 * radius);
+            this.radius = radius;
+            position = Vector2.FromPoint(rect.Location);
+        }
+
+        void UpdateText()
+        {
+            text = (index + 1).ToString();
+            if (treeIndex != -1) text += " (" + (treeIndex + 1) + ")";
+        }
+
+        public void SetTreeIndex(int i)
+        {
+            treeIndex = i;
+            UpdateText();
+        }
+
+        public void Draw(Pen pen, Graphics g)
+        {
+            Brush brush = Brushes.Pink;
+
+            if (opened) brush = Brushes.LightGreen;
+            if (visited) brush = Brushes.LimeGreen;
+            if (current) brush = Brushes.Red;
+
+            g.FillEllipse(brush, rect);
+            g.DrawEllipse(pen, rect);
+            g.DrawString(text, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, (center - new Vector2(1, 0)*4*text.Length - new Vector2(0, 1) * 7).point);
+        }
+
+        public void DrawCircle(Vector2 centerPoint, Pen pen, Graphics g, bool withArrow)
+        {
+            Vector2 dir = (center - centerPoint).normalized;
+            Vector2 p = center + dir * 2 * radius;
+
+            Vector2 p2 = p + dir.turnOn((float)(1.57f)) * 30;
+            Vector2 p3 = p - dir.turnOn((float)(1.57f)) * 30;
+
+            Vector2 p1 = Vector2.FromPoint(GetEdgePoint(p2));
+            Vector2 p4 = Vector2.FromPoint(GetEdgePoint(p3));
+
+            g.DrawBezier(pen, p1.point, p2.point, p3.point, p4.point);
+
+            if (withArrow)
+            {
+                dir = (p3 - p4).normalized;
+                const int h = 10;
+                Point t1 = p4.point;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] { t1, t2, t3 });
+            }
+        }
+
+        public void DrawLineTo(Node node, Pen pen, Graphics g, bool withArrow)
+        {
+            Point p1 = GetEdgePoint(node.center);
+            Point p2 = node.GetEdgePoint(this.center);
+            g.DrawLine(pen, p1, p2);
+
+            if (withArrow)
+            {
+                Vector2 dir = (center - node.center).normalized;
+                const int h = 10;
+                Point t1 = p2;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] {t1, t2, t3});
+            }
+        }
+
+        public void DrawArcTo(Node node, Vector2 op, Pen pen, Graphics g, bool withArrow)
+        {
+            Point p1 = GetEdgePoint(op);
+            Point p2 = node.GetEdgePoint(op);
+
+            g.DrawBezier(pen, p1, op.point, op.point, p2);
+
+            if (withArrow)
+            {
+                Vector2 dir = (op - node.center).normalized;
+                const int h = 10;
+                Point t1 = p2;
+                Point t2 = (Vector2.FromPoint(t1) + dir * h + dir.turnOn((1.57f)) * h / 3).point;
+                Point t3 = (Vector2.FromPoint(t1) + dir * h - dir.turnOn((1.57f)) * h / 3).point;
+
+                g.FillPolygon(Brushes.Black, new Point[] { t1, t2, t3 });
+            }
+        }
+
+        public Vector2 isBetween(Node n1, Node n2)
+        {
+            Vector2 cp = (n2.center + n1.center) / 2;
+            if ((cp - center).length < radius)
+            {
+                if ((cp - center).length < 2)
+                {
+                    cp = center + (n2.center - n1.center).normalized.turnOn((1.57f)) * 2;
+                }           
+
+                return cp;
+            }
+
+            return null;
+        }
+
+        public Vector2 GetOffsetPointOnCircle(Vector2 point, float scale)
+        {
+            if (ContainsPoint(point))
+            {             
+                Vector2 dir = (point - center).normalized;
+                Vector2 op = center + dir * (radius * scale);
+                return op;
+            }
+            return null;
+        }
+
+        public bool ContainsPoint(Vector2 p)
+        {
+            if ((center - p).length <= radius)
+                return true;
+
+            return false;
+        }
+
+        public Point GetEdgePoint(Vector2 point)
+        {
+            return (center + (point - center).normalized * radius).point;
+        }
+    }
+
+    public struct Graph
+    {
+        public List<Node> nodes;
+        public Matrix matrix;
+
+        public Graph(List<Node> nodes, Matrix matrix)
+        {
+            this.nodes = nodes;
+            this.matrix = matrix;
+        }
+    }
+}
